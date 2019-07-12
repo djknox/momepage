4bdeae650ed14c9620882e0d72a1044871dbb30a
DJ Knox <dhknox@gmail.com>
2019-07-12 13:20:29 -0400

    add yesterday() and tomorrow() views

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.today, name='today'),
+    path('', views.today, name='index'),
+    path('today', views.today, name='today'),
+    path('yesterday/', views.yesterday, name='yesterday'),
+    path('tomorrow/', views.tomorrow, name='tomorrow'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,5 +7,21 @@ def today(request):
     View function for home page of site.
     The homepage currently displays Cohort 5's current day information.
     """
-    today = Day.objects.filter(date=datetime.date.today()).first()
-    return render(request, 'index.html', {'today': today})
+    day = Day.objects.filter(date=datetime.date.today()).first()
+    return render(request, 'index.html', {'day': day})
+
+
+def yesterday(request):
+    """
+    View function for 'yesterday' page of site, displaying the previous day's info.
+    """
+    day = Day.objects.filter(date=datetime.date.today() - datetime.timedelta(days = 1)).first()
+    return render(request, 'index.html', {'day': day})
+
+
+def tomorrow(request):
+    """
+    View function for 'tomorrow' page of site, displaying the next day's info.
+    """
+    day = Day.objects.filter(date=datetime.date.today() + datetime.timedelta(days = 1)).first()
+    return render(request, 'index.html', {'day': day})
