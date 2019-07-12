bfdc22a561c4c447e9e2d6ad7e77c71e3d487831
Andy Dearing <andy.dearing@gmail.com>
2019-07-12 17:09:14 -0400

    links at the top display as expected

--- core/views.py
+++ core/views.py
@@ -7,37 +7,76 @@ def today(request):
     View function for home page of site.
     The homepage currently displays Cohort 5's current day information.
     """
-    yesterday = True
-    tomorrow = True
+    previous_day_name = 'yesterday'
+    next_day_name = 'tomorrow'
+    day_name = 'today'
 
     day = Day.objects.filter(date=datetime.date.today()).first()
 
-    if datetime.date.today().weekday() == 0:
-        yesterday = False
+    if is_Monday():
+        previous_day_name = 'Friday'
 
-    if datetime.date.today().weekday() == 4:
-        tomorrow = False
+    if is_Friday():
+        next_day_name = 'Monday'
 
     return render(request, 'index.html', {
         'day': day,
-        'yesterday' : yesterday,
-        'tomorrow' : tomorrow,
+        'dayName': day_name,
+        'previousDayName' : previous_day_name,
+        'nextDayName' : next_day_name,
     })
 
+def is_Monday():
+    """
+    Returns true if it's Monday today
+    """
+    if datetime.date.today().weekday() == 0:
+        return True
+    else :
+        return False
+
+def is_Friday():
+    """
+    Returns true if it's Friday today
+    """
+    if datetime.date.today().weekday() == 4:
+        return True
+    else :
+        return False
 
 def previous_day(request):
     """
     View function for 'previous day' (usually yesterday) page of site, displaying the previous day's info.
     """
-    yesterday = True
+    previous_day_name = ''
+    if is_Monday():
+        day_name = 'Friday'
+        next_day_name = 'Monday'
+    else:
+        day_name = 'yesterday'
+        next_day_name = 'today'
     day = Day.objects.filter(date=datetime.date.today() - datetime.timedelta(days = 1)).first()
-    return render(request, 'index.html', {'day': day})
+    return render(request, 'index.html', {
+        'dayName': day_name,
+        'previousDayName': previous_day_name,
+        'nextDayName': next_day_name
+    })
 
 
 def next_day(request):
     """
     View function for 'next day' page of site (usually tomorrow), displaying the next day's info.
     """
-    tomorrow = True
-    day = Day.objects.filter(date=datetime.date.today() + datetime.timedelta(days = 1)).first()
-    return render(request, 'index.html', {'day': day})
+    next_day_name = ''
+    if is_Friday():
+        day_name = 'Monday'
+        previous_day_name = 'Friday'
+    else:
+        day_name = 'tomorrow'
+        previous_day_name = 'today'
+    day = Day.objects.filter(date=datetime.date.today() - datetime.timedelta(days = 1)).first()
+    return render(request, 'index.html', {
+        'dayName': day_name,
+        'previousDayName': previous_day_name,
+        'nextDayName': next_day_name
+    })
